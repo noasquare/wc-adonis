4fe0f9ae689096b1a82af6f18ce1879effc66309
雅 任 <yaren@RENYAdeMacBook-Air.local>
2018-12-31 18:18:43 +0800

    init

---
+++ start/routes.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+
+const Database = use('Database')
+const Route = use('Route')
+
+Route.on('/').render('welcome')
+
+// Route.get('/hello',({ request }) => {
+//   return `hello ~ ${ request.input('name')}`
+// })
+Route.get('/hello','HelloController.render')
+
+Route.get('/posts', async () => {
+  return await Database.table('posts').select('*')
+})
