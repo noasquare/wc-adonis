0abd3180f3950dec701c43429d5d581df4ce1787
雅 任 <yaren@RENYAdeMacBook-Air.local>
2019-01-01 11:04:20 +0800

    response

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,8 @@
+'use strict'
+
+class PostController {
+  index(){
+     return `post controller is activate!`}
+}
+
+module.exports = PostController
--- start/routes.js
+++ start/routes.js
@@ -23,8 +23,43 @@ Route.on('/').render('welcome')
 // Route.get('/hello',({ request }) => {
 //   return `hello ~ ${ request.input('name')}`
 // })
-Route.get('/hello','HelloController.render')
+// Route.get('/hello','HelloController.render')
+//
+// Route.get('/posts', async () => {
+//   return await Database.table('posts').select('*')
+// })
+// Route.get('/posts',({request}) => request.get())
 
-Route.get('/posts', async () => {
-  return await Database.table('posts').select('*')
+// Route.post('/posts',({request}) => request.post())
+// Route.post('/posts',({request}) => request.all())
+// Route.post('/posts',({request}) => request.collect(['content','title']))
+// Route.post('/posts',({request}) => request.header('user-agent'))
+// const delay = (data,time) => {
+//   return new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//       resolve(data)
+//     },time)
+//   })
+// }
+// Route.get('/posts',async ({request}) => {
+  // const data = await delay(
+    // 'List of posts',
+    // 3000
+  // )
+//   return data
+// })
+Route.get('/list-of-posts',({ response }) => {
+  response.route('list-of-posts')
 })
+Route.get('/list-of-food-posts',({response}) => {
+  response.route('list-of-posts', { category:'food' })
+})
+
+Route.get('/posts/:category?',({ params }) => {
+  return `List of ${ params.category || 'default' } posts.`
+})
+.as ('List-of-posts')
+// Route.get('/posts',() => {
+//   return 'List of posts.'
+// })
+// .as ('list-of-posts')
